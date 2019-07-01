8de1077568478b53558545e8e8bb558632f0aad4
Мохнаткин Алексей <a.mokhnatkin@a-i.kz>
2019-07-01 11:57:37 +0600

    order table creation

--- create_one_table.py
+++ create_one_table.py
@@ -7,16 +7,33 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://dressidea:fjEidk89@loca
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 
-
-class QuestionFromSite(db.Model):#вопрос с сайта
+class Client(db.Model):#карточка клиента
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(50),index=True, nullable=False)
-    phone = db.Column(db.String(20),index=True, nullable=False)    
+    phone = db.Column(db.String(20),index=True,unique=True, nullable=False)
+    insta = db.Column(db.String(50))
+    source_id = db.Column(db.Integer,db.ForeignKey('client_source.id'))
     timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
-    question = db.Column(db.String(1000))
+    comment = db.Column(db.String(200))
+    bookings = db.relationship('Booking',backref='client',lazy='dynamic')
+    visits = db.relationship('Visit',backref='client',lazy='dynamic')
+    visits = db.relationship('Order',backref='client',lazy='dynamic')
+
+    def __repr__(self):
+        return '<Client {}>'.format(self.name)
+
+class Order(db.Model):#мои заказы
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    client_id = db.Column(db.Integer,db.ForeignKey('client.id'))
+    description = db.Column(db.String(1000))
+    begin = db.Column(db.DateTime)
+    end = db.Column(db.DateTime)
+    amount = db.Column(db.Float)
+    timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
 
 #create tables in DB
-QuestionFromSite.__table__.create(db.session.bind, checkfirst=True)
+Order.__table__.create(db.session.bind, checkfirst=True)
 #Video.__table__.create(db.session.bind, checkfirst=True)
 
 #drops tables
